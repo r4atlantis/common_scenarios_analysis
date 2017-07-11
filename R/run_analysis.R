be112f318b0eb65881bd1f2d340ef814c2203292
kellijohnson <kellifayejohnson@gmail.com>
2017-07-11 11:48:20 -0700

    Use run_analysis to calc par values

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -37,14 +37,56 @@ run_analysis <- function(data,
   res.ccfstd <- calc_ccf(data.std, ...)
 
   #' 3. Run MARSS
-  res.marss <- calc_MARSS(data, Q = Qem, R = Rem)
+  tempgrid <- expand.grid(Qem, Rem)
+  res.marss <- apply(tempgrid, 1,
+    function(x) calc_MARSS(data, Q = x[1], R = x[2]))
+
+  #' 4. Get results
+  est <- data.frame(
+    "EM_process" = "",
+    "EM_observation" = "",
+    "par" = c("correlation", paste0("ccf:", res.ccfstd$ccf$lag),
+    "ccf:ar1"),
+    "est" = c(res.corstd$estimate, res.ccfstd$ccf$acf,
+    ifelse("ar1" %in% names(res.ccfstd$xmodel$coef),
+      res.ccfstd$xmodel$coef["ar1"], "yes")))
+
+  B <- cbind(
+    t(sapply(lapply(res.marss, MARSS:::parmat), "[[", "B"))[,
+      c(1, 2, 4), drop = FALSE],
+    t(sapply(lapply(res.marss, MARSS:::parmat), "[[", "Q"))[,
+      c(1, 2, 4), drop = FALSE],
+    t(sapply(lapply(res.marss, MARSS:::parmat), "[[", "R"))[,
+      c(1, 2, 4), drop = FALSE],
+    tempgrid)
+  colnames(B) <- c(
+    "par_Ba:a", "par_Bi:a", "par_Bi:i",
+    "par_Qa:a", "par_Qi:a", "par_Qi:i",
+    "par_Ra:a", "par_Ri:a", "par_Ri:i",
+    "EM_process", "EM_observation")
+  B <- reshape(B, direction = "long",
+    varying = list(1:(NCOL(B) - 2)),
+    v.names = "est", timevar = "par",
+    times = gsub("par_", "", colnames(B))[1:(NCOL(B) - 2)])
+  rownames(B) <- NULL
+  # Remove the zero constrained process errors
+  B <- B[!(B$EM_observation == "zero" &
+    grepl(x = B$par, pattern = "R[a-z]:[a-z]")), ]
+  # Remove the off-diagonals of the identity matrices
+  B <- B[!(grepl(pattern = "diagonal", x = B$EM_observation) &
+    grepl(x = B$par, pattern = "Ri:a")), ]
+  B <- B[!(grepl(pattern = "diagonal", x = B$EM_process) &
+    grepl(x = B$par, pattern = "Qi:a")), ]
+  # Remove the ID
+  B <- B[, -which(colnames(B) == "id")]
 
   res.list <- list("data" = data, "data_std" = data.std,
     "correlation" = res.cor,
     "correlation_std" = res.corstd,
     "ccf" = res.ccf,
     "ccf_std" = res.ccfstd,
-    "marss" = res.marss)
+    "marss" = res.marss,
+    "pars" = merge(est, B, all = TRUE))
 
   return(res.list)
 }
